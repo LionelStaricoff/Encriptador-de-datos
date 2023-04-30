e2daa483f741d70e310474d0c8c4e059d44e9ce1
lionel staricoff <staricofflionel@hotmail.com>
2023-04-30 17:36:35 -0500

    encriptado correcto

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("ola");
+let btnEncriptar = document.querySelector(".encriptar");
+
+function encriptar(){
+    var entrada = document.querySelector("#text-area");
+    alert(entrada.value)
+    let arreglo = entrada.value.split('');
+   let modificado ="";
+    for(arr of arreglo){
+      if (arr=="e"){
+        modificado+="enter"
+      }else if (arr=="i"){
+        modificado+="imes";
+      }else if (arr=="a"){
+        modificado+="ai";
+      }else if (arr=="o"){
+        modificado+="ober";
+      }else if (arr=="u"){
+        modificado+="ufat";
+      }else{
+        modificado+=arr;
+      }
+        
+    }
+    let pantalla = document.querySelector(".datos");
+    pantalla.innerHTML="<h2>"+modificado+"</h2>"
+    
+  
+}
+ 
+function desencriptar(dato){
+
+}
+
+btnEncriptar.onclick=encriptar;
+
+
