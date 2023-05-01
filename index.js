1418565d3c6fc666a70a1806d4c503db1b4b39ce
lionel staricoff <staricofflionel@hotmail.com>
2023-05-01 17:55:56 -0500

    se agrega botón para copiar a la papelera

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let btnEncriptar = document.querySelector(".encriptar");
 
 function encriptar(){
-    var entrada = document.querySelector("#text-area");
-    alert(entrada.value)
+    let entrada = document.querySelector("#text-area");
+   
     let arreglo = entrada.value.split('');
    let modificado ="";
     for(arr of arreglo){
@@ -21,16 +21,72 @@ function encriptar(){
       }
         
     }
-    let pantalla = document.querySelector(".datos");
-    pantalla.innerHTML="<h2>"+modificado+"</h2>"
-    
+    let pantalla = document.querySelector(".mensaje");
+    pantalla.innerHTML='<h2 id="datoH2"> '+modificado+ '</h2>';
+    entrada.value="";
   
 }
  
-function desencriptar(dato){
+function desencriptar(){
+  let entrada = document.querySelector("#text-area");
+   
+  let arreglo = entrada.value.split('');
+ let modificado ="";
+ let encrypt="";
+
+ for (arr of arreglo){
+
+      if(arr=="e" || arr=="n" || arr=="t" || arr=="r"|| arr=="i"|| arr=="m"|| arr=="s"
+      || arr=="a"|| arr=="o"|| arr=="b"|| arr=="u"|| arr=="f"|| arr=="t" ){
+        encrypt+=arr;
+      }
+
+ }
+
+
+
+function vrevertirClave(arr){
+  let dato="";
+  if (arr=="enter"){
+    dato="e"
+  }else if (arr=="imes"){
+    dato="i";
+  }else if (arr=="ai"){
+    dato="a";
+  }else if (arr=="ober"){
+    dato="o";
+  }else if (arr=="ufat"){
+    dato="u";
+  }else{
+    dato=arr;
+  }
+  return dato;
+}
+
+
+
 
 }
 
-btnEncriptar.onclick=encriptar;
+function copyToClipBoard() {
+
+    var content = document.querySelector("#text-area");
+    //alert(content.value)
+    content.select();
+    document.execCommand("copy");
+    content.value="";
+    
+    }
+
+
+
+
+
+    let btnCopiar = document.querySelector(".copiar")
+    let btnDesencriptar = document.querySelector(".desencriptar")
+
+    btnCopiar.onclick=copyToClipBoard;
+    btnEncriptar.onclick=encriptar;
+    btnDesencriptar.onclick=desencriptar;
 
 
