15909c3e4849381a10bcf17bc6e66293e650df06
lionel staricoff <staricofflionel@hotmail.com>
2023-05-02 15:57:30 -0500

    cambiando la funcion decodificar

--- index.js
+++ index.js
@@ -26,27 +26,56 @@ function encriptar(){
     entrada.value="";
   
 }
+
+
  
 function desencriptar(){
   let entrada = document.querySelector("#text-area");
    
-  let arreglo = entrada.value.split('');
+ let arr = entrada.value.split('');
  let modificado ="";
  let encrypt="";
 
- for (arr of arreglo){
 
-      if(arr=="e" || arr=="n" || arr=="t" || arr=="r"|| arr=="i"|| arr=="m"|| arr=="s"
-      || arr=="a"|| arr=="o"|| arr=="b"|| arr=="u"|| arr=="f"|| arr=="t" ){
-        encrypt+=arr;
+   
+    //verifica y esta las palabras sifradas 
+   for (let i=0; i<arr.length; i++){
+    //verifica si esta enter
+      if(arr[i]=="e" && arr[i+1]=="n" && arr[i+2]=="t" && arr[i+3]=="e" && arr[i+4]=="r"){
+        encrypt="enter";
+        i=i+4;
+        // verifica imes
+      }else if(   arr[i]=="i"&& arr[i+1]=="m"&& arr[i+2]=="e" && arr[i+3]=="s"){
+        encrypt="imes";
+        i=i+3;
+        //verifica ai
+      }else if(arr[i]=="a"&& arr[i+1]=="i"){
+        encrypt="ai";
+        i=i+1;
+        //verifica ober
+      }else if(arr[i]=="o"&& arr[i+1]=="b"&& arr[i+2]=="e"&& arr[i+3]=="r"){
+        encrypt="ober";
+        i=i+3;
+        //verifica ufat
+      }else if(arr[i]=="u"&& arr[i+1]=="f"&& arr[i+2]=="a" &&  arr[i+3]=="t"){
+        encrypt="ufat";
+        i=i+3;
+      }else{
+        encrypt+=arr[i];
       }
+     
+         encrypt=vevertirClave(encrypt);
+         modificado +=encrypt;
+         encrypt="";
 
  }
+ let pantalla = document.querySelector(".mensaje");
+ pantalla.innerHTML='<h2 id="datoH2"> '+modificado+ '</h2>';
+ entrada.value="";
+}
 
-
-
-function vrevertirClave(arr){
-  let dato="";
+function vevertirClave(arr){
+  let dato=arr;
   if (arr=="enter"){
     dato="e"
   }else if (arr=="imes"){
@@ -57,8 +86,6 @@ function vrevertirClave(arr){
     dato="o";
   }else if (arr=="ufat"){
     dato="u";
-  }else{
-    dato=arr;
   }
   return dato;
 }
@@ -66,7 +93,7 @@ function vrevertirClave(arr){
 
 
 
-}
+
 
 function copyToClipBoard() {
 
